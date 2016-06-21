39701b8cdfdc2b6f846f3a355f686a37edec8d12
joe <w5752947@hotmail.com>
2016-06-21 10:31:03 +0800

    Add files via upload

---
+++ sqlclient.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace sqlclient
+{
+    /// <summary>
+    /// 连接数据库
+    /// 1.先建立sqlconnection
+    /// 2.设置连接字符串，再用open打开链接
+    /// 3.操作完成后，关闭数据库连接
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection con = new SqlConnection();
+            //链接字符串格式一定要对Data Source可以选localhost或IP或本地的实例名称，
+            //Integrated Security=SSPI 表示用本地登录计算机的账户访问数据库（优先选择）可以通过用户名密码链接user id=  password= ，
+            //链接字符串中可以设置连接池的大小Max Pool Size 和Min Pool Size  ,Polling开启连接池 .net中默认开启
+            string constring = String.Format(@"Data Source=.\SQLEXPRESS;Initial Catalog=lol;Integrated Security=SSPI;");
+            con.ConnectionString = constring;
+            //为保证性能 ， 先设置 再打开数据库，无论怎么操作，最后一定要关闭数据库
+            try
+            {                
+                con.Open();
+                Console.WriteLine(con.ServerVersion.ToString());
+                Console.WriteLine(con.State.ToString());
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                con.Close();
+                Console.WriteLine(con.State.ToString());
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
---
+++ xml.cs
@@ -0,0 +1,63 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace xml
+{
+    /// <summary>
+    /// xml读写操作
+    /// 1.先创建xmldocment
+    /// 2.写入/读取xml
+    /// 3.关闭xml
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //写xml,创建xmldocment对象并写入xml
+            XmlDocument xmlDoc = new XmlDocument();
+            //设置xml属性
+            XmlDeclaration xmlDec = xmlDoc.CreateXmlDeclaration("1.0", "utf-8", null);
+            //将属性加入到xml中
+            xmlDoc.AppendChild(xmlDec);
+            //写入根元素，一个xml文件只能有一个根元素
+            XmlElement xmlEle = xmlDoc.CreateElement("books");
+            xmlDoc.AppendChild(xmlEle);
+            //写入根下的元素
+            XmlElement xmlEle2 = xmlDoc.CreateElement("book");
+            xmlEle2.InnerText="第一本书";
+            xmlEle.AppendChild(xmlEle2);
+
+            XmlElement xmlEle3 = xmlDoc.CreateElement("book");
+            xmlEle.AppendChild(xmlEle3);
+            XmlElement xmlEle4 = xmlDoc.CreateElement("titlt");
+            xmlEle4.InnerText = "标题";
+            xmlEle3.AppendChild(xmlEle4);
+            //保存xml文件
+            xmlDoc.Save(@"D:\test.xml");
+
+            try
+            {
+                xmlDoc.Load(@"D:\test.xml");
+                XmlElement rootele = xmlDoc.DocumentElement;
+                XmlNodeList nodelist = rootele.GetElementsByTagName("book");
+                foreach(XmlNode node in nodelist)
+                {
+                    Console.WriteLine(node.Name);
+                    Console.WriteLine(node.InnerText);
+                }
+            }
+            catch(Exception  e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
